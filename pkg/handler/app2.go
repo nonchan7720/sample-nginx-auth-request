e9aac2885bd0d7a0cc77a897bd76836e415bd87c
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-10-04 10:22:27 +0000

    アクセスログとAPIエンドポイントの追加

--- pkg/handler/app2.go
+++ pkg/handler/app2.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/base64"
+	"log/slog"
 	"net/http"
 	"path/filepath"
 
@@ -11,9 +12,13 @@ import (
 func NewApp2Server() *gin.Engine {
 	sessionMap = map[string]loginUser{}
 	engine := gin.New()
+	engine.Use(func(ctx *gin.Context) {
+		slog.Info("Request.", "Path", ctx.Request.URL.Path)
+	})
 	dir := GetDirectory()
 	engine.LoadHTMLGlob(filepath.Join(dir, "statics/app2/html/*"))
 	engine.GET("/app", app2LoginPageHandler)
+	engine.GET("/api/app", app2APIAppHandler)
 	return engine
 }
 
@@ -21,4 +26,9 @@ func app2LoginPageHandler(c *gin.Context) {
 	token := c.GetHeader("X-Access-Token")
 	decodeToken, _ := base64.StdEncoding.DecodeString(token)
 	c.HTML(http.StatusOK, "test.html", gin.H{"Text": string(decodeToken)})
+}
+
+func app2APIAppHandler(c *gin.Context) {
+	c.SetCookie(sessionKeyName, "", -1, "", "", false, true)
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
 }
